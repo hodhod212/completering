78838c1f586f3ace378fdf5075758561cfc30801
Ali Ezadkhaha <43235023+hodhod212@users.noreply.github.com>
2018-12-11 22:33:54 +0100

    Jag har flyttat stylen från kundvagn.js till kundvagn.css

--- mockups/kundvagn.js
+++ mockups/kundvagn.js
@@ -28,25 +28,21 @@ function  displayCart() {
     document.getElementById("show-cart").innerHTML = "";
     for (var i in cartArray) {
         var product = document.createElement("li")
-        product.setAttribute('style','font-size:smaller;border-radius:5px; border:1px solid  #5596F5;display:inline-block;background-color:#F5F5F5;color:black;margin:4px;width:200px;')
         product.setAttribute('class','test');
         var myImage = document.createElement('img');
         myImage.setAttribute('src','assets/'+cartArray[i].price+'.png');
         product.appendChild(myImage);
-        myImage.setAttribute('style',"display: block; margin-left: auto; margin-right: auto;")              
         var h4 = document.createElement('h4');
         h4.innerText = cartArray[i].name ;
         product.appendChild(h4);
         var Input = document.createElement('input');
         Input.setAttribute('class','item-count');
         Input.setAttribute('type','number');
-        Input.setAttribute('style','width:33px;height:19px;border-radius:3px;display:inline;');
         Input.setAttribute('data-name',  cartArray[i].name );
         Input.setAttribute('value',  cartArray[i].count ); 
         product.appendChild(Input);
         var p2 = document.createElement('p');
         p2.setAttribute('type','number');
-        p2.setAttribute('style','display:inline;');
         p2.setAttribute('data-name',  cartArray[i].name );
         p2.setAttribute('value',  cartArray[i].price );
         p2.innerHTML= " x " + cartArray[i].price+  " = " + cartArray[i].total.toFixed(2)+ " kr "+'<br>';
@@ -55,19 +51,16 @@ function  displayCart() {
         Button1.setAttribute('data-name', cartArray[i].name);
         Button1.setAttribute('class', 'plus-item');
         Button1.innerHTML = '+';
-        Button1.setAttribute('style','background-color: #E64E4E;color:white;border-radius:5px 0 0 5px;')
         product.appendChild(Button1);
         var Button2 = document.createElement('button');
         Button2.setAttribute('data-name', cartArray[i].name);
         Button2.setAttribute('class', 'subtract-item');  
         Button2.innerHTML = '-';
-        Button2.setAttribute('style','background-color: #E64E4E;color:white;')
         product.appendChild(Button2); 
         var Button3 = document.createElement('button');
         Button3.setAttribute('class', 'delete-item');
         Button3.setAttribute('data-name',cartArray[i].name);
-        Button3.innerHTML ="<i class='fa fa-trash-o' style='font-size:16px;color:white'> Ta bort</i>";
-        Button3.setAttribute('style','background-color:#E64E4E;color:white;border-radius:0 5px 5px 0;')
+        Button3.innerHTML ="<i class='fa fa-trash-o'> Ta bort</i>";
         product.appendChild(Button3);
         document.getElementById("show-cart").appendChild(product); }
 $("#show-cart").on("click", ".delete-item", function(event){ 
