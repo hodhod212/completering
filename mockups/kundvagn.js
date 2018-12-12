69ebfde04406ed00032dbc700945c96664b871e7
Ali Ezadkhaha <43235023+hodhod212@users.noreply.github.com>
2018-12-12 14:01:19 +0100

    Jag är klar med alla andringar som krävdes.

--- mockups/kundvagn.js
+++ mockups/kundvagn.js
@@ -1,12 +1,5 @@
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+
 function klar() {
     alert("Du har slutfört ditt köp.");
    // $(".show-cart li").remove();
